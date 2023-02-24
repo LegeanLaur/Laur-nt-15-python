859c62d89e35bc8b6f03c5e5589e18aa3da41cee
LegeanLaur <125303910+LegeanLaur@users.noreply.github.com>
2023-02-24 17:43:51 +0200

    Add files via upload
    Ultimele 3 exercitii sunt partial terminate. N-am reusit sa extrag in totalitate numerele pare, impare si toti multiplii de 3. Cate 1 numar in fiecare sir, a fost "intrusul".

---
+++ Homework 1.py
@@ -0,0 +1,9 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print(my_list)
+print(my_list[:: -1])
+my_slice = my_list [1:10:2]
+print(my_slice)
+my_slice = my_list [0::2]
+print(my_slice)
+my_slice = my_list [2::7]
+print(my_slice)
